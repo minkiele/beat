b1302fd75f18e0ee8e24db3be42a175d92e13b37
Michele Montagner <michelasso@gmail.com>
2015-02-09 00:45:36 +0100

    Added standard deviation

--- public/js/app.js
+++ public/js/app.js
@@ -27,33 +27,95 @@
       }
     };
 
-    this.getBpm = function(){
-      var i, avg = 0;
-      for(i = 0; i < this.avgBuffer.length; i++){
+    this.getAvg = function(){
+      var i, avg = 0, bufferSize = this.getBufferSize();
+      for(i = 0; i < bufferSize; i++){
         avg += this.avgBuffer[i];
       }
 
-      avg /= this.avgBuffer.length;
-
-      return 60 * 1000 / avg;
+      avg /= bufferSize;
+      
+      return avg;
     };
     
+    this.getBpm = function(){
+      return 60 * 1000 / this.getAvg();
+    };
+
     this.reset = function(){
       this.avgBuffer = [];
       this.avgBufferIndex = 0;
+      this.timeBuffer = null;
     };
     
     this.reset();
 
+    this.getBufferSize = function(){
+      return Math.min(this.avgBufferSize, this.avgBuffer.length);
+    };
+    
+    this.setBufferSize = function(size){
+      this.avgBufferSize = size;
+      this.reset();
+    }
+    
+    this.getStdDev = function(){
+      var i, sqAvg = Math.pow(this.getAvg(), 2), bufferSize = this.getBufferSize(), stdDev = 0;
+      for(i = 0; i < bufferSize; i++){
+        stdDev += Math.pow(this.avgBuffer[i], 2);
+      }
+
+      stdDev /= bufferSize;
+      stdDev -= sqAvg;
+
+      stdDev = Math.pow(stdDev, 0.5);
+
+      return stdDev;
+    };
+    
+    this.getBpmStdDev = function(){
+      // K = 60000 (ms per minute)
+      // A = AVG S = STDDEV C = BPM AVG
+      // B = C - K/(A+S);
+      // C = K/A;
+      // B = K/A - K/(A+S);
+      // B = (K(A+S) - KA)/(A(A+S))
+      // B = KS/(A(A+S))
+      var stdDev = this.getStdDev(),
+          avg = this.getAvg();
+      return stdDev * 60 * 1000 / (avg * (avg + stdDev));
+    };
+
   }
 
-  angular.module('beatFinder', [])
+  angular.module('beatFinder', ['rt.debounce'])
     .service('beat', Beat)
-    .controller('BeatController', ['$scope', 'beat', function($scope, beat){
+    .controller('BeatController', ['$scope', 'beat', 'debounce', function($scope, beat, debounce){
+      var inactivityReset;
+
       $scope.bpm = 0;
+      $scope.buffer = 10;
+      $scope.stddev = 0;
+      
       $scope.updateBpm = function(){
         beat.addBeat();
         $scope.bpm = beat.getBpm();
+        $scope.stddev = beat.getBpmStdDev();
+        inactivityReset();
       };
+
+      $scope.$watch('buffer', function(newValue, oldValue){
+        beat.setBufferSize(parseInt(newValue));
+      });
+
+      $scope.reset = function(){
+        beat.reset();
+        $scope.updateBpm();
+      };
+
+      inactivityReset = debounce(3000, function(){
+        $scope.reset();
+      });
+
     }]);
 }();
--- web.js
+++ web.js
@@ -3,5 +3,5 @@ var app = express();
 
 app.set('port', (process.env.PORT || 8888));
 app.use(express.static(__dirname + '/public'));
-
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.listen(app.get('port'));
